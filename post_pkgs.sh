9bc27adacad8a8cb4473fce664157b77c71ec847
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-19 04:15:36 -0600

    Update post_pkgs.sh

--- post_pkgs.sh
+++ post_pkgs.sh
@@ -13,6 +13,10 @@ sudo sh -c "echo 'permit nopass root' >> /etc/doas.conf"
 sudo sh -c "echo 'permit nopass :wheel cmd reboot' >> /etc/doas.conf"
 sudo sh -c "echo 'permit nopass :wheel cmd poweroff' >> /etc/doas.conf"
 
+# Necessary Fonts
+pkgs = "terminus-font ttf-terminus-nerd ttf-liberation ttf-liberation-mono-nerd ttf-dejavu-nerd ttf-noto-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd"
+doas pacman -S $pkgs
+
 # Clone necessary repositories and install
 git clone https://github.com/siduck/chadwm --depth 1 ~/.config/chadwm
 cd ~/.config/chadwm/chadwm && doas make clean install && cd ~/
