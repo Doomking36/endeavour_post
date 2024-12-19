fd9db3803c92366fa5f42e4790b32adc54a160fa
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-19 04:03:17 -0600

    Update and rename template.sh to post_pkgs.sh

---
+++ post_pkgs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Updating system
+sudo pacman -Syu
+
+# Install necessary packages
+pkgs = "git doas neovim sx fastfetch imlib2 alsa-lib alsa-utils picom feh rofi firefox"
+sudo pacman -S $pkgs
+
+# Create doas.conf
+sudo sh -c "echo 'permit persist :wheel' >> /etc/doas.conf"
+sudo sh -c "echo 'permit nopass root' >> /etc/doas.conf"
+sudo sh -c "echo 'permit nopass :wheel cmd reboot' >> /etc/doas.conf"
+sudo sh -c "echo 'permit nopass :wheel cmd poweroff' >> /etc/doas.conf"
+
+# Clone necessary repositories and install
+git clone https://github.com/siduck/chadwm --depth 1 ~/.config/chadwm
+cd ~/.config/chadwm/chadwm && doas make clean install && cd ~/
+cp ~/.config/chadwm/scripts/run.sh ~/
+
+git clone https://github.com/Doomking36/st
+cd st && doas make clean install && cd ~/
+
+git clone https://github.com/nvim-lua/kickstart.nvim ~/.config/nvim
+
+echo "Post-installation setup is complete. Please reboot your system."
