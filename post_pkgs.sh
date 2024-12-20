65b8792fa4f9fb0fa72a2328432c42ac38cf6633
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-20 17:45:56 -0600

    Update post_pkgs.sh

--- post_pkgs.sh
+++ post_pkgs.sh
@@ -4,8 +4,8 @@
 sudo pacman -Syu
 
 # Install necessary packages
-pkgs = "git doas neovim sx fastfetch imlib2 alsa-lib alsa-utils xcompmgr feh rofi firefox emacs"
-sudo pacman -S $pkgs
+packages = "git doas neovim sx fastfetch imlib2 alsa-lib alsa-utils xcompmgr feh rofi firefox emacs"
+sudo pacman -S $packages
 
 # Create doas.conf
 sudo sh -c "echo 'permit persist :wheel' >> /etc/doas.conf"
@@ -15,7 +15,7 @@ sudo sh -c "echo 'permit nopass :wheel cmd poweroff' >> /etc/doas.conf"
 
 # Necessary Fonts
 pkgs = "terminus-font ttf-terminus-nerd ttf-liberation ttf-liberation-mono-nerd ttf-dejavu-nerd ttf-noto-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd"
-doas pacman -S $pkgs
+doas pacman -S $packages
 
 # Clone necessary repositories and install
 git clone https://github.com/siduck/chadwm --depth 1 ~/.config/chadwm
@@ -26,5 +26,7 @@ git clone https://github.com/Doomking36/st
 cd st && doas make clean install && cd ~/
 
 git clone https://github.com/nvim-lua/kickstart.nvim ~/.config/nvim
+
+doas ln -s /usr/bin/nvim /usr/bin/vim
 
 echo "Post-installation setup is complete. Please reboot your system."
